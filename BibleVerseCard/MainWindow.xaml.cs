0a9862bcd9a950d479ed792bb6c3ed6c47d95a0d
Jacob North <jnorth.ao@gmail.com>
2022-10-31 22:10:24 -0400

    Cleaning up the project to start rebuilding the logic woth a local DB

--- BibleVerseCard/MainWindow.xaml.cs
+++ BibleVerseCard/MainWindow.xaml.cs
@@ -11,63 +11,48 @@ namespace BibleVerseCard
         public MainWindow()
         {
             InitializeComponent();
-            //SetVerseInWindowAsync();
-            //Title = "Memorize by Copying";
+            SetVerseInWindowAsync();
+            Title = "Memorize by Copying";
         }
 
         private void btn_Check_Click(object sender, RoutedEventArgs e)
         {
-            //bool doMatch = CheckMatch();
-            //if (doMatch)
-            //{
-            //    MessageBox.Show("Good Job! \n\n The verses matched. \n\n Goodbye.", "Success");
-            //    System.Windows.Application.Current.Shutdown();
-            //}
-            //else
-            //    MessageBox.Show("Oops! \n\n The verses don't match. \n\n Please try again.", "Failed");
+            bool doMatch = CheckMatch();
+            if (doMatch)
+            {
+                MessageBox.Show("Good Job! \n\n The verses matched. \n\n Goodbye.", "Success");
+                System.Windows.Application.Current.Shutdown();
+            }
+            else
+                MessageBox.Show("Oops! \n\n The verses don't match. \n\n Please try again.", "Failed");
         }
 
         private void btn_New_Verse_Click(object sender, RoutedEventArgs e)
         {
-            //SetVerseInWindowAsync();
+            SetVerseInWindowAsync();
         }
 
-        //private bool CheckMatch()
-        //{
-        //    bool match = false;
+        private bool CheckMatch()
+        {
+            bool match = false;
 
-        //    int CompareBook = string.Compare(V_Book.Text, C_Book.Text.Trim(), comparisonType: StringComparison.OrdinalIgnoreCase);
-        //    int CompareChapter = string.Compare(V_Chapter.Text, C_Chapter.Text.Trim(), comparisonType: StringComparison.OrdinalIgnoreCase);
-        //    int CompareText = string.Compare(V_Text.Text, C_Text.Text.Trim(), comparisonType: StringComparison.OrdinalIgnoreCase);
+            int CompareBook = string.Compare(V_Book.Text, C_Book.Text.Trim(), comparisonType: StringComparison.OrdinalIgnoreCase);
+            int CompareChapter = string.Compare(V_Chapter.Text, C_Chapter.Text.Trim(), comparisonType: StringComparison.OrdinalIgnoreCase);
+            int CompareText = string.Compare(V_Text.Text, C_Text.Text.Trim(), comparisonType: StringComparison.OrdinalIgnoreCase);
 
-        //    if (CompareBook == 0) 
-        //        if(CompareChapter == 0) 
-        //            if(CompareText == 0)
-        //                match = true;
-        //    else
-        //        match = false;
+            if (CompareBook == 0)
+                if (CompareChapter == 0)
+                    if (CompareText == 0)
+                        match = true;
+                    else
+                        match = false;
 
-        //    return match;
-        //}
+            return match;
+        }
 
-        //public async Task SetVerseInWindowAsync()
-        //{
-        //    BibleAPI bibleAPI = new BibleAPI();
-        //    Verses verse = new Verses();
-        //    ResponseWithVerse newReference = await bibleAPI.GetVerse();
-        //    foreach(var v in newReference.Response.Verses) { verse = v; }
-        //    if(newReference != null)
-        //    {
-        //        V_Book.Text = verse.Book_Name.Trim().Replace("\n", String.Empty);
-        //        V_Chapter.Text = verse.Chapter.Trim().Replace("\n", String.Empty);
-        //        V_Text.Text = newReference.Verse + " " + verse.Text.Trim().Replace("\n", String.Empty);
-        //    }
-        //    else
-        //    {
-        //        V_Book.Text = "Unknown";
-        //        V_Chapter.Text = "Error";
-        //        V_Text.Text = "This Didn't work!";
-        //    }
-        //}
+        public async Task SetVerseInWindowAsync()
+        {
+
+        }
     }
 }
